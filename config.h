4e349a07f44bd805df5370fa2c6156b38e14a8d0
Michal Šindelář <michalsindelar03@gmail.com>
2016-03-10 23:15:54 +0100

    NEW: Added support for variables from CMAKE

---
+++ config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// define your version_libinterface
+#define PROJECT_DIR "/Users/michal/Library/Caches/CLion12/cmake/generated/4e465258/4e465258/RelWithDebInfo1"
+
+
+#endif // CONFIG_H
